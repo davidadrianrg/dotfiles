f37a6c6b68b68c07db8c0696ed88f4a30b0d50af
David Adrián Rodríguez García <davidadrianrg@gmail.com>
2023-02-09 19:21:41 +0100

    Change mega backup to root user

--- README.md
+++
@@ -1,2 +0,0 @@
-# dotfiles
-My dotfile managed by chezmoi
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,7 +5,7 @@ end
 # Aliases
 
 alias cat "batcat"
-alias bck "watch mega-backup -lh"
+alias bck "sudo watch mega-backup -lh"
 alias dca "sudo docker-compose -f docker-compose.yml -f docker-compose.caddy.yml up -d"
 alias fixlag "sudo iw wlp2s0 set power_save off"
 alias apt "nala"
